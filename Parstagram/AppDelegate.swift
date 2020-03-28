931421af2501ac8131601529403252c6f547f133
Kevin Prakasa <kevinprakasa15@gmail.com>
2020-03-27 23:53:29 -0700

    added func to add comment and save it to Parse

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -11,6 +11,7 @@ import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -20,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://lit-mesa-99611.herokuapp.com/parse"
             })
         )
+        
         return true
     }
 
